b040a6e05ed076491ec86e7fc21c1bf670cfde58
Patricia Polts <patricia.polts@positiva.eco.br>
2023-12-13 23:03:20 -0300

    update img title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ export function Header() {
             <div className='cotainer w-full '>
                 <div className="grid grid-cols-1 grid-rows-1 gap-4 auto-cols-auto">
                     <div className='flex flex-row flex-nowrap col-span-2' style={{ alignContent: 'space-around', justifyContent: 'flex-start', alignItems: 'center' }}>
-                        <figure className='overflow-hidden w-20 h-20' tabIndex={1} title="Patricia Poltts - Fullstack freelancer.">
+                        <figure className='overflow-hidden w-20 h-20' tabIndex={1} title="Pati Poltts - Fullstack freelancer.">
                             <a href='https://github.com/patpolts' target='_blank'>
-                                <Image className='rounded-full w-full h-full' src="/imgs/me.jpg" width={100} height={100} priority alt="Foto Perfil" title="Clique para ir para o meu github" />
+                                <Image className='rounded-full w-full h-full' src="/imgs/me.jpg" width={100} height={100} priority alt="Foto Perfil" title="Clique para acessar meu github" />
                             </a>
                         </figure>
                         <hgroup tabIndex={2}>
