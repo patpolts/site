473817d9c2c4009479ffbac8c46493d7a9748575
Patricia Polts <patricia.polts@positiva.eco.br>
2023-12-13 21:19:11 -0300

    tailwind custom setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,49 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx,html}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx,html}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx,html}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'arial', 'sans-serif'],
+      serif: ['Montserrat', 'arial', 'serif'],
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '.125rem',
+      DEFAULT: '.25rem',
+      'lg': '.5rem',
+      'full': '100%',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem'
+      },
+      width: {
+        'w-11': '4rem'
+      },
+      extend: {
+        backgroundImage: {
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+          'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        },
       },
     },
-  },
-  plugins: [],
+    plugins: []
+  }
 }
+
 export default config
