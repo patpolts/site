89e5c680d3c230737950ff0c23e83c0d18a805eb
Patricia Polts <patricia.polts@positiva.eco.br>
2023-12-13 22:57:22 -0300

    update columns grid

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ export function Header() {
     return (
         <header tabIndex={0} className='z-1'>
             <div className='cotainer w-full '>
-                <div className="grid grid-cols-5 grid-rows-1 gap-4 auto-cols-auto">
-                    <div className='flex flex-row flex-nowrap col-span-1' style={{ alignContent: 'space-around', justifyContent: 'flex-start', alignItems: 'center' }}>
-                        <figure className='overflow-hidden w-16 h-16 ' tabIndex={1} title="Patricia Poltts - Fullstack freelancer.">
+                <div className="grid grid-cols-1 grid-rows-1 gap-4 auto-cols-auto">
+                    <div className='flex flex-row flex-nowrap col-span-2' style={{ alignContent: 'space-around', justifyContent: 'flex-start', alignItems: 'center' }}>
+                        <figure className='overflow-hidden w-20 h-20' tabIndex={1} title="Patricia Poltts - Fullstack freelancer.">
                             <a href='https://github.com/patpolts' target='_blank'>
                                 <Image className='rounded-full w-full h-full' src="/imgs/me.jpg" width={100} height={100} priority alt="Foto Perfil" title="Clique para ir para o meu github" />
                             </a>
@@ -15,9 +15,6 @@ export function Header() {
                             <h1>Pati Poltts</h1>
                             <h2>Fullstack Developer</h2>
                         </hgroup>
-                    </div>
-                    <div className='col-span-3   text-right'>
-
                     </div>
 
                 </div>
