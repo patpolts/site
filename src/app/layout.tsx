d3c8270ed00a9f73feaab2ecc21ed6ba535f540d
Patricia Poltts <patriciapoltts@gmail.com>
2023-12-14 21:44:37 -0300

    build and font config uypdates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter, Montserrat } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
-import { Header } from '../components/Header'
+import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
-export const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
-})
 
 export const metadata: Metadata = {
   title: 'Pati Polts » Fullstack Developer',
@@ -21,10 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
+      <body >
         <div className='container text-center bg-center  min-w-full min-h-screen p-4'>
           <Header />
           {children}
+          <Footer />
         </div>
       </body>
     </html>
